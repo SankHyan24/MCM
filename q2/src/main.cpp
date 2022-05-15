49077865c26761164daacf04fe7aaebea403e5ee
Charles Sun <2209339564@qq.com>
2022-05-15 21:54:53 +0800

    data correct

--- q2/src/main.cpp
+++ q2/src/main.cpp
@@ -4,36 +4,79 @@
 #include <vector>
 #include <algorithm>
 #include <utility.hpp>
+#include <assert.h>
 using namespace std;
 
 vector<string> words;
-vector<char> letters;
 int freq[28][28];
+int letter[28];
 
 void read_recipe()
 {
-    ifstream fin("../data/letters.txt");
-    char tmp;
-    while (fin >> tmp)
-        letters.push_back(tmp);
-    cout << letters.size();
+    ifstream fin("../data/word_refined.txt");
+    string word;
+    while (fin >> word)
+        words.push_back(word);
     fin.close();
+    cout << "Read " << words.size() << " words." << endl;
+
     for (int i = 0; i < 28; i++)
         for (int j = 0; j < 28; j++)
             freq[i][j] = 0;
-    for (auto i = letters.begin(); (i + 1) != letters.end(); i++)
-    {
-        auto j = i + 1;
-        freq[*i - 'a'][*j - 'a']++;
-    }
-    cout << "reading done" << endl;
-    ofstream fout("../data/freq.txt");
-    for (int i = 0; i < 28; i++)
-    {
-        for (int j = 0; j < 28; j++)
-            fout << setw(5) << freq[i][j] << ",";
-        fout << endl;
-    }
+    //统计字母组合
+    // for (auto &word : words)
+    // {
+    //     for (int i = 0; i < word.size(); ++i)
+    //     {
+    //         for (int j = i + 1; j < word.size(); ++j)
+    //         {
+    //             freq[word[i] - 'a'][word[j] - 'a']++;
+    //             freq[word[j] - 'a'][word[i] - 'a']++;
+    //         }
+    //         freq[word[i] - 'a'][26]++;
+    //         freq[26][word[i] - 'a']++;
+    //     }
+    // }
+
+    // 统计字频
+    // for (auto &word : words)
+    // {
+    //     for (int i = 0; i < word.size(); ++i)
+    //     {
+    //         letter[word[i] - 'a']++;
+    //     }
+    //     letter[26]++;
+    // }
+
+    // // 统计单方向的相邻字母组合
+    // for (auto &word : words)
+    // {
+    //     freq[26][word[0] - 'a']++;
+    //     for (int i = 0; i < word.size() - 1; ++i)
+    //         freq[word[i] - 'a'][word[i + 1] - 'a']++;
+    //     freq[word[word.size() - 1] - 'a'][26]++;
+    // }
+
+    // ofstream fout("../data/newn.txt");
+    // for (int i = 0; i < 28; ++i)
+    // {
+    //     for (int j = 0; j < 28; ++j)
+    //         fout << setw(6) << freq[i][j] << ',';
+    //     fout << endl;
+    //     // fout << setw(6) << letter[i] << ',';
+    // }
+
+    // // newn check
+    // for (int i = 0; i < 28; ++i)
+    // {
+    //     int sum = 0;
+    //     for (int j = 0; j < 28; ++j)
+    //         sum += freq[i][j];
+    //     int sum_ = 0;
+    //     for (int j = 0; j < 28; ++j)
+    //         sum_ += freq[j][i];
+    //     assert(sum == sum_);
+    // }
 }
 inline int myabs(int x)
 {
@@ -42,15 +85,5 @@ inline int myabs(int x)
 
 int main()
 {
-    // read_recipe();
-    for (int i = 0; i < 28; i++)
-        for (int j = 0; j < 28; j++)
-            freq[i][j] = myabs((i - j) / 7 + (i - j) % 7);
-    ofstream fout("../data/dist.txt");
-    for (int i = 0; i < 28; i++)
-    {
-        for (int j = 0; j < 28; j++)
-            fout << setw(5) << freq[i][j] << ",";
-        fout << endl;
-    }
+    read_recipe();
 }
